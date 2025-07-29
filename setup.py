ac2f85d291baa251542c61658f457f8d60606242
Aditya Deshmukh <aditya.22310607@viit.ac.in>
2025-07-29 10:10:33 +0530

    prototype done

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import find_packages, setup
 
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aditya Deshmukh",
+    author_email="adityadeshmukh1298@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
